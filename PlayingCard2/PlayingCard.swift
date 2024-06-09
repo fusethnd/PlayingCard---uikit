a7207e6fa2c29629ebb0d7e938b8b7c977e82d33
fusethnd <fusethnd@gmail.com>
2024-06-09 22:52:48 +0700

    added some more

--- PlayingCard2/PlayingCard.swift
+++ PlayingCard2/PlayingCard.swift
@@ -5,25 +5,66 @@
 //  Created by Thanadon Boontawee on 9/6/2567 BE.
 //
 
-import UIKit
+import Foundation
 
-class PlayingCard: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+struct PlayingCard: CustomStringConvertible {
+    var description: String {
+        return "\(rank)\(suit)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+//        var description: String
+        
+        case spades = "♠️"
+        case hearts = "♥️"
+        case clubs = "♣️"
+        case diamonds = "♦️"
+        
+        static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description: String { return rawValue }
     }
-    */
+    
+    enum Rank: CustomStringConvertible {
+//        var description: String
+        
+        case ace // A
+        case face(String) // J,Q,K
+        case numeric(Int) // 2-10
 
+        var order: Int {
+            switch self {
+                case .ace: return 1
+                case .numeric(let pips): return pips
+                // return number directly
+                case .face(let kind) where kind == "J": return 11
+                case .face(let kind) where kind == "Q": return 12
+                case .face(let kind) where kind == "K": return 13
+                default: return 0
+            }
+        }
+        
+        static var all: [Rank] { // when all method is called
+            var allRanks = [Rank.ace]
+            // start allRanks value with 1 item - array that value is ace
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+                // append 2-10 value into allRanks
+            }
+            allRanks += [Rank.face("J"),.face("Q"),.face("K")]
+            // appends J,Q,K at last and return array
+            return allRanks
+        }
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind) : return kind
+            }
+        }
+    }
 }
--- PlayingCard2/ViewController.swift
+++ PlayingCard2/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var deck = PlayingCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
     }
 
 
 }
 
+extension CGFloat {
+    var arc4random: CGFloat {
+        if self > 0 {
+            return CGFloat(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -CGFloat(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
